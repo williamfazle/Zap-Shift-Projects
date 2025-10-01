c8a406cb2c2db7b2769b606b56896bc70e9482f6
Fazle Rabby <fazlerabby272@gmail.com>
2025-10-02 01:32:05 +0600

    Using react-hook-from in Login component

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = data =>{
+        console.log(data);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                  <fieldset className="fieldset">
 
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" {...register('email')} className="input" placeholder="Email" />
 
 
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" {...register('password', {required: true, minLength: 6})}  className="input" placeholder="Password" />
 
-          
+
           <div><a className="link link-hover">Forgot password?</a></div>
 
 
